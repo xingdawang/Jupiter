d6e30a841bde7d8ba3a0d86df85378a66ea8e4ff
Xingda Wang <wxd598113636@gmail.com>
2015-02-26 16:43:52 +0000

    Add OKJupiter system entrance into the system; link Github engine to the OKJupiter bash

---
+++ OKJupiter.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#This is the entrance of the whole programme
+#@auther: Xingda Wang
+#@date: 26th/Feb/2015
+#param $1 the target code language
+#param $2 correspondence operation
+
+#basic color with bold type
+WhiteText="\033[37m"					#bold white and white have same effect on some platform e.g Ubuntu
+BoldRedText="\033[1;31m"				#bold red text
+RedText="\033[31m"					#red text
+BoldGreenText="\033[1;32m"				#bold green text
+GreenText="\033[32m"					#green text
+BoldYellowText="\033[1;33m"				#bold yellow text
+YellowText="\033[33m"					#yellow text
+BoldBlueText="\033[1;34m"				#bold blue text
+BlueText="\033[34m"					#blue text
+BoldMagentaText="\033[1;35m"				#bold magenta text
+MagentaText="\033[35m"					#magenta text
+BoldCyanText="\033[1;36m"				#bold cyan text
+CyanText="\033[36m"					#cyan text
+escapeText="\033[0m"
+
+#Welcome Board
+echo -e "$BoldGreenText********************************************************************************$escapeText"
+echo
+echo -e "			$BlueText Welcome to use Jupiter $escapeText"
+echo
+echo -e "$BoldYellowText""Jupiter is a data mining tool designed for analysis the version histories of software systems, thus help to improve software quality. $escapeText"
+echo
+echo -e "$BoldYellowText""Here is the entrance the whole program, and you can manipulate the whole application through this entrance. $escapeText"
+echo
+echo -e "$BoldYellowText""Only$escapeText""$BoldRedText two$escapeText""$BoldYellowText parameter need to be provived, like the format below: $escapeText"
+echo
+echo -e "		$BoldGreenText ./OKJupiter java getFile $escapeText""		""$YellowText(case insensitive)$escapeText"
+echo
+echo -e "$BoldYellowText""For more information see manul please. (e.g. ./OKJupiter java manual) $escapeText"
+echo
+echo -e "$BoldGreenText********************************************************************************$escapeText"
+
+#record the operation into the log
+cd core.Xingda.Jupiter/					#take the launch information into the record		
+sh LogRecorder.sh "load Jupiter"
+cd ..
+
+#judge the parameters
+if [[ -z $1 && -z $2 ]];then				#judge whether the paramters have been entered
+	echo -e "$RedText""Please Enter the paramter like the above format: $escapeText"
+elif [[ -z $2 ]];then					#if the second parameter is missing, remind the user
+	echo -e "$RedText""Missing the second parameter. $escapeText"
+fi
+
+#take command from the user
+if [[ $1 && $2 ]];then					#if both parameter is entered
+	
+	#JAVA						#Java related
+	temp1=$1					#${temp1,,} can change all $1 characters into lower case
+	if [[ ${temp1,,} == "java" ]];then		
+		temp2=$2
+		if [[ ${temp2,,} == "getfile" ]];then	#get file from github
+			#echo $PWD
+			cd core.Xingda.Jupiter/CloudEngine
+			#ls
+			sh GithubEngine.sh
+		fi
+
+	#Add further language here	
+	#elif [[ $1 -eq "future_language" ]];then
+	#	do something
+
+	else
+		echo -e "$MagentaText""Do not support the input programming language $escapeText"
+	fi
+fi
+
--- core.Xingda.Jupiter/CloudEngine/GithubEngine.sh
+++ core.Xingda.Jupiter/CloudEngine/GithubEngine.sh
@@ -6,41 +6,31 @@
 #@date:	18/02/2015
 
 #Fetch file system from a given url
-function fetchFile {
-	printf "Please enter url: "
-	read url
-	cd ..								#move to core.Xingda.Jupiter folder
-	sh LogRecorder.sh "Reading url from $url"			#take down log
-	cd CloudEngine							#move back to the current folder
-	echo $(git clone $url)						#clone file from remote server
-}
+printf "Please enter url: "
+read url
+cd ..									#move to core.Xingda.Jupiter folder
+sh LogRecorder.sh "Reading url from $url"				#take down log
+cd CloudEngine								#move back to the current folder
+echo $(git clone $url)							#clone file from remote server
+#printf "You are in the folder: "
+#echo $PWD								#find the current folder
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )			#deprecated
+#echo $DIR								#deprecated
 
-#After fetched the file, detect the folder
-function detectFolder {
-	fetchFile							#run fetchFile function first
-	printf "You are in the folder: "
-	echo $PWD							#find the current folder
-	#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )		#deprecated
-	#echo $DIR							#deprecated
+#Transfer current file system to the src folder
+tempString=$(ls)							#read current directory file
+fileArray=${tempString}							#separate name by white space (/s)
 
-	tempString=$(ls)						#read current directory file
-	fileArray=(${tempString})					#separate name by white space (/s)
-#	echo ${#fileArray[@]}						#file length
-	for i in "${!fileArray[@]}"					#add "!" to indicate content rather than an index number
-	do								#itrate to check whether a file is a folder
-		if [ -d ${fileArray[i]} ];then				#if it is a directory (-d)
-			echo "${fileArray[i]}" "is a directory, and will be moved to src directory."
-			echo $(mv ${fileArray[i]} ../../src.Xingda.Jupiter)
-			cd ..						#move to core.Xingda.Jupiter folder
-			sh LogRecorder.sh "Moving ${fileArray[i]} to src.Xingda.Jupiter"
-			cd CloudEngine	
-		else
-			echo "${fileArray[i]} is not a directory."
-		fi
-	done
-	
-}
-
-#Currently, only two functions are running
-detectFolder 								#(embed fetchFile)
-#fetchFile
+#echo ${#fileArray[@]}							#file length
+for i in $fileArray							#add "!" to indicate content rather than an index number
+do									#itrate to check whether a file is a folder
+	if [ -d $i ];then						#if it is a directory (-d)
+		echo "$i is a directory, and will be moved to src directory."
+		echo $(mv $i ../../src.Xingda.Jupiter)
+		cd ..							#move to core.Xingda.Jupiter folder
+		sh LogRecorder.sh "Moving $i to src.Xingda.Jupiter"
+		cd CloudEngine	
+	else
+		echo "$i is not a directory."
+	fi
+done
